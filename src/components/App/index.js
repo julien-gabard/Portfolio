2a8f8bc2dbec532bbfce998cc37effa97731956f
julien-gabard <juliengabard@hotmail.fr>
2020-07-26 20:28:17 +0200

    deleted react logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,12 @@
 // == Import npm
 import React from 'react';
 
-// == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+// == Import scss
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
   </div>
 );
