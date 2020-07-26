3398926ee3ea834f0258411acd5fbb7ac797230e
julien-gabard <juliengabard@hotmail.fr>
2020-07-26 20:17:27 +0200

    add folder reducer and files

---
+++ src/reducers/boardReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  // Todo...
+};
+
+const boardReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
+
+export default boardReducer;
