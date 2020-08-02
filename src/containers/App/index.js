e76039649d0ab87ec280ac20bcc116e316696129
julien-gabard <juliengabard@hotmail.fr>
2020-08-02 17:33:41 +0200

    deleted com

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -9,12 +9,10 @@ import App from 'src/components/App';
 
 // == Fonction props
 const mapStateToProps = (state) => ({
-  // nom de la prop à remplir: donnée à récupérer dans le state
   displayBoard: state.app.displayBoard,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  // nom de la prop à remplir: callback qui contient un appel à dispatch
   onHandleClick: () => {
     dispatch(menuButtonAction());
   },
