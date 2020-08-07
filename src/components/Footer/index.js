2ea91da938f32e5590bebc29603937929200d082
julien-gabard <juliengabard@hotmail.fr>
2020-08-07 16:34:12 +0200

    fix icon footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,8 +13,11 @@ import { faLinkedin, faGithubSquare, faFacebookSquare } from '@fortawesome/free-
 // == Component
 const Footer = ({ showIcons, clickShowIcons }) => {
   let animeIconLinkedin = 'footer__list-link-linkedin';
+  let linkLinkedin = '';
   let animeIconGithub = 'footer__list-link-github';
+  let linkGithub = '';
   let animeIconFacebook = 'footer__list-link-facebook';
+  let linkFacebook = '';
 
   const handleClick = () => {
     clickShowIcons();
@@ -22,8 +25,11 @@ const Footer = ({ showIcons, clickShowIcons }) => {
 
   if (showIcons === true) {
     animeIconLinkedin = 'footer__list-link-linkedin show__linkedin';
+    linkLinkedin = 'https://www.linkedin.com/in/julien-gabard';
     animeIconGithub = 'footer__list-link-github show__github';
+    linkGithub = 'https://github.com/julien-gabard';
     animeIconFacebook = 'footer__list-link-facebook show__facebook';
+    linkFacebook = 'https://www.facebook.com/julien.gabard.3/';
   }
 
   return (
@@ -33,13 +39,13 @@ const Footer = ({ showIcons, clickShowIcons }) => {
         <FontAwesomeIcon icon={faShareAlt} className="footer__user-icone" />
       </div>
       <div className="footer__list">
-        <a href="https://www.linkedin.com/in/julien-gabard" className={animeIconLinkedin}>
+        <a href={linkLinkedin} className={animeIconLinkedin}>
           <FontAwesomeIcon icon={faLinkedin} className="footer__list-link-icone" />
         </a>
-        <a href="https://github.com/julien-gabard" className={animeIconGithub}>
+        <a href={linkGithub} className={animeIconGithub}>
           <FontAwesomeIcon icon={faGithubSquare} className="footer__list-link-icone github" />
         </a>
-        <a href="https://www.facebook.com/julien.gabard.3/" className={animeIconFacebook}>
+        <a href={linkFacebook} className={animeIconFacebook}>
           <FontAwesomeIcon icon={faFacebookSquare} className="footer__list-link-icone" />
         </a>
       </div>
