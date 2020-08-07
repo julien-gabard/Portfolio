c9a987abb1e013a4678c928c8be04f70ebb2d9da
julien-gabard <juliengabard@hotmail.fr>
2020-08-07 18:16:09 +0200

    anime AOS about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -19,38 +19,50 @@ const About = () => (
         <h2 className="about-content__title-content">A propos</h2>
         <div className="about-content__title-line" />
       </div>
-      <p className="about-content__content">
-        Bonjour moi c'est Julien, développeur web curieux, rigoureux et autonome.
-      </p>
-      <p className="about-content__content">
-        En novembre 2019, j'ai entamé une reconversion dans le domaine du développement web
-        en suivant une formation intensive chez
-        <a href="https://oclock.io/?gclid=EAIaIQobChMIkoOAtvX56gIVCPhRCh1QLAvnEAAYASAAEgJ7afD_BwE" className="about-content__content-link"> O'clock </a>
-        qui ces terminer en mai 2020.
-      </p>
-      <p className="about-content__content">
-        Après, de nombreuse années dans la mécanique auto et dans la vente commerciale,
-        il était temps pour moi de me tourner vers un domaine qui me passionne depuis
-        de nombreuse années et où j'allais m'épanouir.
-      </p>
-      <p className="about-content__content">
-        Passionné par l'univers du web, j'ai naturellement choisi de me spécialiser dans les
-        langages axés UI plus particulièrement React.js.
-      </p>
+      <div data-aos="zoom-in-left" data-aos-duration="2000">
+        <p className="about-content__content">
+          Bonjour moi c'est Julien, développeur web curieux, rigoureux et autonome.
+        </p>
+      </div>
+      <div data-aos="zoom-in-right" data-aos-duration="2000">
+        <p className="about-content__content">
+          En novembre 2019, j'ai entamé une reconversion dans le domaine du développement web
+          en suivant une formation intensive chez
+          <a href="https://oclock.io/?gclid=EAIaIQobChMIkoOAtvX56gIVCPhRCh1QLAvnEAAYASAAEgJ7afD_BwE" className="about-content__content-link"> O'clock </a>
+          qui ces terminer en mai 2020.
+        </p>
+      </div>
+      <div data-aos="zoom-in-left" data-aos-duration="2000">
+        <p className="about-content__content">
+          Après, de nombreuse années dans la mécanique auto et dans la vente commerciale,
+          il était temps pour moi de me tourner vers un domaine qui me passionne depuis
+          de nombreuse années et où j'allais m'épanouir.
+        </p>
+      </div>
+      <div data-aos="zoom-in-right" data-aos-duration="2000">
+        <p className="about-content__content">
+          Passionné par l'univers du web, j'ai naturellement choisi de me spécialiser dans les
+          langages axés UI plus particulièrement React.js.
+        </p>
+      </div>
     </div>
-    <div className="about-picture">
-      <img src={pictureProfile} alt="julien" className="about-picture__img" />
+    <div data-aos="zoom-in" data-aos-duration="2000">
+      <div className="about-picture">
+        <img src={pictureProfile} alt="julien" className="about-picture__img" />
+      </div>
     </div>
-    <div className="about__link-list">
-      <a href="https://www.linkedin.com/in/julien-gabard">
-        <FontAwesomeIcon icon={faLinkedin} className="about__link-list-icone" />
-      </a>
-      <a href="https://github.com/julien-gabard">
-        <FontAwesomeIcon icon={faGithubSquare} className="about__link-list-icone github" />
-      </a>
-      <a href="https://www.facebook.com/julien.gabard.3/">
-        <FontAwesomeIcon icon={faFacebookSquare} className="about__link-list-icone" />
-      </a>
+    <div data-aos="fade-right" data-aos-offset="100" data-aos-easing="ease-in-sine">
+      <div className="about__link-list">
+        <a href="https://www.linkedin.com/in/julien-gabard">
+          <FontAwesomeIcon icon={faLinkedin} className="about__link-list-icone" />
+        </a>
+        <a href="https://github.com/julien-gabard">
+          <FontAwesomeIcon icon={faGithubSquare} className="about__link-list-icone github" />
+        </a>
+        <a href="https://www.facebook.com/julien.gabard.3/">
+          <FontAwesomeIcon icon={faFacebookSquare} className="about__link-list-icone" />
+        </a>
+      </div>
     </div>
   </div>
 );
